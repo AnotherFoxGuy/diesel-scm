66695b38bc41e8114d45f02cba3db397cec365eb
Kostas <Kostas@noemail.net>
2015-08-26 09:58:28 +0000

    Added build revision information and clickable links to about box
    FossilOrigin-Name: 6c585dffad9f240a390a241dd71f5f2f95f46d49

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -8,12 +8,29 @@ AboutDialog::AboutDialog(QWidget *parent, const QString &fossilVersion) :
 {
 	ui->setupUi(this);
 	QString banner(QCoreApplication::applicationName() + " " + QCoreApplication::applicationVersion());
-	ui->lblBanner->setText(banner + "\n" + ui->lblBanner->text());
+	banner += "\n" + ui->lblBanner->text();
+	banner += "\n<a href=\"https://fuel-scm.org/\">https://fuel-scm.org/</a>";
+	banner = banner.replace("\n", "<br/>");
+	ui->lblBanner->setText(banner);
+	ui->lblBanner->setOpenExternalLinks(true);
+	ui->lblBanner->setTextFormat(Qt::RichText);
 
 	ui->lblQtVersion->setText(tr("QT version %0").arg(QT_VERSION_STR));
 
 	if(!fossilVersion.isEmpty())
 		ui->lblFossilVersion->setText(tr("Fossil version %0").arg(fossilVersion));
+
+	QString revisiontxt;
+	QFile fmanifest(":/version/manifest");
+	if(fmanifest.open(QFile::ReadOnly))
+	{
+		QString revision = QString(fmanifest.readAll()).trimmed();
+		revisiontxt = QString(tr("Fuel revision %0").arg("<a href=\"https://fuel-scm.org/fossil/timeline?c=%0\">%0</a>").arg(revision));
+		ui->lblFuelRevision->setOpenExternalLinks(true);
+		ui->lblFuelRevision->setTextFormat(Qt::RichText);
+		fmanifest.close();
+	}
+	ui->lblFuelRevision->setText(revisiontxt);
 
 	QString additional;
 	QFile ftrans(":/docs/docs/Translators.txt");
